cc4237e25f209de7ac75d0b1a62cae2bb8b00845
Carl Norton <Carlmundo@users.noreply.github.com>
2026-08-26 13:18:53 +0100

    Diff update and creation bat

---
+++ Diff/create.bat
@@ -0,0 +1,3 @@
+xdelta3.exe -e -f -s .\105UK\frontend.exe ..\Patch\Languages\English\frontend.exe .\105UK\frontend-en.vcdiff
+xdelta3.exe -e -f -s .\GOG\frontend.exe ..\Patch\Languages\English\frontend.exe .\GOG\frontend-en.vcdiff
+pause
